5745acb176899296badad981d3cbd049b48efd57
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-08-27 00:53:37 +0200

    feat: scaffold basic class structure

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class LinkedList {
+  constructor() {
+    this._head = null;
+    this._tail = null;
+    this._length = 0;
+  }
+}
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
