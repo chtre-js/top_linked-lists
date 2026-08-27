b919931e6d856bb48e0b6fbe41bb5e3e12877f77
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-08-27 12:35:23 +0200

    test(linkedList): add test for append function on an empty linked list

---
+++ index.spec.js
@@ -0,0 +1,9 @@
+import LinkedList from "./index.js";
+
+test("append a value to an empty list change the head", () => {
+  const linkedList = new LinkedList;
+
+  linkedList.append(800);
+
+  expect(linkedList._head.value).toEqual(800);
+})
