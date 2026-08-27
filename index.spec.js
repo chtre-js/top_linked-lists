7f8a804186c18bf7e233b1b39688fa6e8c42ef54
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-08-27 13:29:21 +0200

    test: add test to appending several values and add skipped test for prepend method base case

--- index.spec.js
+++ index.spec.js
@@ -14,9 +14,30 @@ test("append a value to an empty list increment the length to 1", () => {
   expect(linkedList.listLength).toEqual(1);
 });
 
+test("append several values update the tail", () => {
+  const linkedList = new LinkedList;
+  linkedList.append(700);
+  linkedList.append(600);
+  linkedList.append(500);
+
+  expect(linkedList.tailValue).toEqual(500);
+})
+
 test("prepend a value to an empy list adds a head", () => {
   const linkedList = new LinkedList();
   linkedList.prepend(800);
 
   expect(linkedList.headValue).toEqual(800);
+})
+
+test.skip("prepend a value to a list changes the head and link to the former head", () => {
+  const linkedList = new LinkedList;
+  linkedList.append(100);
+  linkedList.append(200);
+  linkedList.append(300);
+  
+  linkedList.prepend(800);
+
+  expect(linkedList.headValue).toEqual(800);
+  expect(linkedList._head.nextNode.value).toEqual(100);
 })
