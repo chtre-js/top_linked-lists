efe9bf6975eae0972318bfaef01ce0ea90a319f1
Cherif <227361371+chtre-js@users.noreply.github.com>
2026-08-27 13:36:49 +0200

    test(LinkedList): verify icrementation of size after several appendings and fix size getter name

--- index.spec.js
+++ index.spec.js
@@ -11,7 +11,7 @@ test("append a value to an empty list increment the length to 1", () => {
   const linkedList = new LinkedList();
   linkedList.append(700);
 
-  expect(linkedList.listLength).toEqual(1);
+  expect(linkedList.size).toEqual(1);
 });
 
 test("append several values update the tail", () => {
@@ -21,6 +21,15 @@ test("append several values update the tail", () => {
   linkedList.append(500);
 
   expect(linkedList.tailValue).toEqual(500);
+})
+
+test("append several values update the size accordingly", () => {
+  const linkedList = new LinkedList;
+  linkedList.append(700);
+  linkedList.append(600);
+  linkedList.append(500);
+
+  expect(linkedList.size).toEqual(3);
 })
 
 test("prepend a value to an empy list adds a head", () => {
